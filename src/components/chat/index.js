3d83d014b590dd1b028bdf07739ea6913e27e203
Addy056 <aiaera056@gmail.com>
2026-07-31 15:43:27 +0530

    pages updated

---
+++ src/components/chat/index.js
@@ -0,0 +1,7 @@
+export { default as ChatWidget } from "./ChatWidget";
+export { default as ChatHeader } from "./ChatHeader";
+export { default as ChatInput } from "./ChatInput";
+export { default as ChatMessages } from "./ChatMessages";
+export { default as MessageBubble } from "./MessageBubble";
+export { default as QuickActions } from "./QuickActions";
+export { default as TypingIndicator } from "./TypingIndicator";
