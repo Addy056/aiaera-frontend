c456efb4bc9c0d8f10a7557da137a485977710e6
Addy056 <aiaera056@gmail.com>
2026-08-03 23:20:46 +0530

    pages updated

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -317,7 +317,8 @@ export default function MainLayout() {
         </div>
 
         <div className="mx-auto w-full max-w-[1440px] px-4 py-4 sm:px-6 lg:px-8 lg:py-5 xl:px-10">
-        
+          <Outlet />
+
         </div>
       </main>
     </div>
